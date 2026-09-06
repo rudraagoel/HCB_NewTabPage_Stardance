1f6ff2ecadeb80e31d131e7db4b280423fc1b738
Rudra Goyel <rudragoel2012@gmail.com>
2026-09-06 20:13:01 +0530

    Shortcuts Complete!

--- index.js
+++ index.js
@@ -40,3 +40,101 @@ function updateDigitalClock() {
         // Run clock immediately and update every second
         updateDigitalClock();
         setInterval(updateDigitalClock, 1000);
+
+const button = document.getElementById("add-shortcut");
+const shortcutList = document.getElementById("shortcut-list");
+
+const defaultShortcuts = [
+    {
+        url: "https://hackclub.com"
+    },
+    {
+        url: "https://ysws.hackclub.com"
+    },
+    {
+        url: "https://github.com"
+    },
+    {
+        url: "https://stardance.hackclub.com"
+    }
+];
+
+let shortcuts = JSON.parse(localStorage.getItem("shortcuts"));
+
+if (!shortcuts) {
+    shortcuts = defaultShortcuts;
+    localStorage.setItem("shortcuts", JSON.stringify(shortcuts));
+}
+
+
+button.addEventListener("click", function () {
+
+    const url = prompt("Enter website URL:");
+
+    if (!url) {
+        return;
+    }
+
+    let websiteURL = url.trim();
+
+    if (!websiteURL.startsWith("http://") &&
+        !websiteURL.startsWith("https://")) {
+
+        websiteURL = "https://" + websiteURL;
+    }
+
+    shortcuts.push({
+        url: websiteURL
+    });
+
+    localStorage.setItem(
+        "shortcuts",
+        JSON.stringify(shortcuts)
+    );
+
+    loadShortcuts();
+});
+
+
+function loadShortcuts() {
+
+    shortcutList.innerHTML = "";
+
+    shortcuts.forEach(function(shortcut, index) {
+
+        const shortcutElement = document.createElement("div");
+
+        shortcutElement.className = "shortcut";
+
+        shortcutElement.innerHTML = `
+            <a href="${shortcut.url}" target="_blank">
+                <img
+                    src="https://www.google.com/s2/favicons?domain=${shortcut.url}&sz=64"
+                    alt="SHORTCUT"
+                >
+            </a>
+
+            <button class="remove-shortcut" type="button">×</button>
+        `;
+
+        const removeButton =
+            shortcutElement.querySelector(".remove-shortcut");
+
+        removeButton.addEventListener("click", function() {
+
+            shortcuts.splice(index, 1);
+
+            localStorage.setItem(
+                "shortcuts",
+                JSON.stringify(shortcuts)
+            );
+
+            loadShortcuts();
+        });
+
+        shortcutList.appendChild(shortcutElement);
+    });
+}
+
+
+loadShortcuts();
